ba879554b27419aa946fe9075e9d7f9fef22a3ea
zackprogrammer <63200308+zackprogrammer@users.noreply.github.com>
2021-08-02 21:31:01 -0400

    Removed unload, reload and loads command.
    Removed unload, reload and loads command. Those are dangerous.

--- main.py
+++ main.py
@@ -6,22 +6,6 @@ import os
 bot = commands.Bot(command_prefix=':')
 bot.remove_command('help')
 
-@bot.command()
-@commands.has_permissions(administrator=True)
-async def load(ctx, extension):
-    bot.load_extension(f'cogs.{extension[:-3]}')
-
-
-@bot.command()
-@commands.has_permissions(administrator=True)
-async def unload(ctx, extension):
-    bot.unload_extension(f'cogs.{extension}')
-
-@bot.command()
-@commands.has_permissions(administrator=True)
-async def reload(ctx, extension):
-    bot.unload_extension(f'cogs.{extension}')
-    bot.load_extension(f'cogs.{extension}')
 
 for filename in os.listdir('cogs'):
     if filename.endswith('.py'):
