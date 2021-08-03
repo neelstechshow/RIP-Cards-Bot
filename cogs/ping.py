cedd685d11936ada4b8b750bd1ddeb934d915a49
Programming-Done-Right <84484122+Programming-Done-Right@users.noreply.github.com>
2021-08-02 18:15:13 -0700

    Create ping.py

---
+++ cogs/ping.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+
+class ping(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if str(message.content) == '<@!844066483982172200>':
+            embed = discord.Embed(title='My Prefix Is :', color = discord.Color.from_rgb(0, 191, 178))
+            await message.channel.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(ping(bot))
+    
